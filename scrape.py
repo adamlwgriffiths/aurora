3821dca20508cfb69ef9441face274b252cac2cf
Adam Griffiths <adam.lw.griffiths@gmail.com>
2015-10-30 09:33:20 +1100

    Simplify code

--- scrape.py
+++ scrape.py
@@ -25,6 +25,7 @@ def serialise(im):
 
 def download_image(url):
     response = requests.get(url, stream=True)
+    response.raise_for_status()
 
     parser = ImageFile.Parser()
     for chunk in response.iter_content(1024):
@@ -38,87 +39,33 @@ def download_image(url):
 
 def save_image(url, key):
     print('Downloding {}'.format(url))
-    #filename = os.path.basename(url)
-    #path = os.path.join(directory, filename)
-
     im = download_image(url)
     store.put(key, im)
-    #with open(path, 'wb') as f:
-    #    f.write(im)
 
 
-aurora_1 = None
-aurora_2 = None
-aurora_3 = None
-aurora_directory = 'aurora/'
-
-
-def scrape_aurora():
-    url = 'http://www.antarctica.gov.au/webcams/aurora'
-    global aurora_1
-    global aurora_2
-    global aurora_3
+def scrape_webcam(camera):
+    url = 'http://www.antarctica.gov.au/webcams/{}'.format(camera)
 
     response = requests.get(url)
     soup = BeautifulSoup(response.content, "html.parser")
-    a1 = soup.find('a', id='view1')['href']
-    a2 = soup.find('a', id='view2')['href']
-    a3 = soup.find('a', id='view3')['href']
-    if a1 != aurora_1:
-        aurora_1 = a1
-        save_image(a1, aurora_directory + os.path.basename(a1))
-    if a2 != aurora_2:
-        aurora_2 = a2
-        save_image(a2, aurora_directory + os.path.basename(a2))
-    if a3 != aurora_3:
-        aurora_3 = a3
-        save_image(a3, aurora_directory + os.path.basename(a3))
 
-casey = None
-casey_directory = 'casey/'
-def scrape_casey():
-    url = 'http://www.antarctica.gov.au/webcams/casey'
-    global casey
+    urls = set(
+        map(lambda x: x['href'], soup.find_all('a', href=webcam_re)) +
+        map(lambda x: x['src'], soup.find_all('img', src=webcam_re))
+    )
 
-    response = requests.get(url)
-    soup = BeautifulSoup(response.content, "html.parser")
-    img = soup.find('img', src=webcam_re)['src']
-    if img != casey:
-        casey = img
-        save_image(img, casey_directory + os.path.basename(img))
-
-davis = None
-davis_directory = 'davis/'
-def scrape_davis():
-    url = 'http://www.antarctica.gov.au/webcams/davis'
-    global davis
-
-    response = requests.get(url)
-    soup = BeautifulSoup(response.content, "html.parser")
-    img = soup.find('img', src=webcam_re)['src']
-    if img != davis:
-        davis = img
-        save_image(img, davis_directory + os.path.basename(img))
-
-mawson = None
-mawson_directory = 'mawson/'
-def scrape_mawson():
-    url = 'http://www.antarctica.gov.au/webcams/mawson'
-    global mawson
-
-    response = requests.get(url)
-    soup = BeautifulSoup(response.content, "html.parser")
-    img = soup.find('img', src=webcam_re)['src']
-    if img != mawson:
-        mawson = img
-        save_image(img, mawson_directory + os.path.basename(img))
+    for image in urls:
+        try:
+            save_image(image, '{}/{}'.format(camera, os.path.basename(image)))
+        except:
+            print('Failed to download image {}'.format(image))
 
 
 def run():
-    scrape_aurora()
-    scrape_casey()
-    scrape_davis()
-    scrape_mawson()
+    scrape_webcam('aurora')
+    scrape_webcam('casey')
+    scrape_webcam('davis')
+    scrape_webcam('mawson')
 
 
 def scrape():
