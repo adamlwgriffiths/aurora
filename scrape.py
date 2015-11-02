53b675e24471986e4789e27a5ff9ffd09e73bc4e
Adam Griffiths <adam.lw.griffiths@gmail.com>
2015-11-02 13:01:47 +1100

    Add historic scrape for aurora and bases.
    Some information is hard coded atm.

--- scrape.py
+++ scrape.py
@@ -2,6 +2,7 @@ from __future__ import absolute_import, print_function
 import re
 import os
 import time
+import datetime
 import requests
 from bs4 import BeautifulSoup
 import kvstore
@@ -76,6 +77,87 @@ def scrape_webcam(camera):
             print('Failed to download image {}'.format(image))
 
 
+def historic_aurora(image):
+    base = 'aurora'
+    base_code = image[0]
+    image_day = int(image[1:-4])
+    image_hour = int(image[-4:-2])
+    image_minute = int(image[-2:])
+
+    t_orig = t = datetime.datetime(2015, 11, 2, image_hour, image_minute)
+
+    # the first image i managed to capture
+    cameras = ['A', 'B', 'C']
+
+    min_t = datetime.datetime(2015, 10, 29)
+    while t > min_t:
+        t_new = t
+
+        for camera in cameras:
+            # the camera sometimes gets images that are after the 30m marks
+            # so poke about +10 minutes
+            for x in range(10):
+                try:
+                    day_code = image_day - (t_orig - t_new).days
+
+                    image = 'http://images.antarctica.gov.au/webcams/{base}/15/{code}{number}{time}{camera}.jpg'.format(
+                        base=base,
+                        code=base_code,
+                        number=day_code,
+                        time=t_new.strftime('%H%M'),
+                        camera=camera,
+                    )
+                    save_image(image, '{}/{}'.format(base, os.path.basename(image)))
+                    break
+                except:
+                    print('Failed to download image {}'.format(image))
+                    t_new += datetime.timedelta(minutes=1)
+
+        t_new = t - datetime.timedelta(minutes=30)
+        #if t_new.day != t.day:
+        #    image_day -= 1
+        t = t_new
+
+
+def historic_base(base, path):
+    year, month, day, image = path.split('/')
+    base_code = image[0]
+    image_day = int(image[1:-4])
+    image_hour = int(image[-4:-2])
+    image_minute = int(image[-2:])
+
+    t_orig = t = datetime.datetime(int(year), int(month), int(day), image_hour, image_minute)
+
+    min_t = datetime.datetime(2015, 10, 29)
+    while t > min_t:
+        day_code = image_day - (t_orig - t).days
+        image = 'http://images.antarctica.gov.au/webcams/{base}/{date}/{code}{number}{time}s.jpg'.format(
+            base=base,
+            date=t.strftime('%Y/%m/%d'),
+            code=base_code,
+            number=day_code,
+            time=t.strftime('%H%M'),
+        )
+        try:
+            save_image(image, '{}/{}'.format(base, os.path.basename(image)))
+        except:
+            print('Failed to download image {}'.format(image))
+        t_new = t - datetime.timedelta(minutes=5)
+        #if t_new.day != t.day:
+        #    image_day -= 1
+        t = t_new
+
+
+def historic_bases():
+    casey = '2015/11/02/C1511020120'
+    davis = '2015/11/02/D1511020120'
+    mawson = '2015/11/01/M1511012355'
+
+    historic_base('casey', casey)
+    historic_base('davis', davis)
+    historic_base('mawson', mawson)
+
+
 def run():
     scrape_webcam('aurora')
     scrape_webcam('casey')
@@ -90,4 +172,6 @@ def scrape():
 
 
 if __name__ == '__main__':
-    run()
+    #run()
+    historic_aurora('A153020000')
+    #historic_bases()
