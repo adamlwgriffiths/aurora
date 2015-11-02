5679ef49ae8b80b1c644eb6daaa65baef0721998
Adam Griffiths <adam.lw.griffiths@gmail.com>
2015-11-02 11:54:44 +1100

    Bases are at 5m, so download previous images too

--- scrape.py
+++ scrape.py
@@ -11,8 +11,8 @@ from PIL import ImageFile
 webcam_re = re.compile(r'/webcams/.*\.jpg', flags=re.I)
 
 s3_bucket = 'antarctica-scrape'
-store = kvstore.create('s3://'+s3_bucket)
-#store = kvstore.create('file://' + os.path.join(os.path.dirname(__file__), 'kvstore'))
+#store = kvstore.create('s3://'+s3_bucket)
+store = kvstore.create('file://' + os.path.join(os.path.dirname(__file__), 'kvstore'))
 
 
 def serialise(im):
@@ -49,10 +49,25 @@ def scrape_webcam(camera):
     response = requests.get(url)
     soup = BeautifulSoup(response.content, "html.parser")
 
-    urls = set(
-        map(lambda x: x['href'], soup.find_all('a', href=webcam_re)) +
-        map(lambda x: x['src'], soup.find_all('img', src=webcam_re))
-    )
+    urls = set(map(lambda x: x['href'], soup.find_all('a', href=webcam_re)))
+
+    if not urls:
+        urls = set(map(lambda x: x['src'], soup.find_all('img', src=webcam_re)))
+
+        # single cam's are generally every 5 minutes
+        # so ensure we got the previous 5 minute image too
+        cam_re = re.compile(r'(?P<prefix>/\D)(?P<number>\d+)(?P<postfix>\D\.jpg)$', flags=re.I)
+        for image in list(urls):
+            try:
+                match = cam_re.search(image)
+                number = int(match.group('number'))
+                prefix, postfix = match.group('prefix'), match.group('postfix')
+                previous = cam_re.sub(prefix + str(number - 5) + postfix, image)
+                next = cam_re.sub(prefix + str(number - 5) + postfix, image)
+                urls.add(previous)
+                urls.add(next)
+            except:
+                pass
 
     for image in urls:
         try:
